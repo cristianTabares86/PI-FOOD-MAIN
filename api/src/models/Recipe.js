29e7e4ac5cd6108ad7b5df8c2d939ea8ad8b2a69
cristianTabares86 <cristiantabares.iq@gmail.com>
2023-02-08 10:41:19 -0500

    Model Recipes v1.0.0

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,25 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    recipeSummary: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    healthScore: {
+      type: DataTypes.FLOAT,
+    },
+    stepByStep: {
+      type: DataTypes.TEXT,
     },
   });
 };
