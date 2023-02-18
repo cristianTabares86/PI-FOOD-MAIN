3aa582e46ba09429200b316f23457bed672a0356
cristianTabares86 <cristiantabares.iq@gmail.com>
2023-02-18 16:12:16 -0500

    General files

---
+++ api/src/controllers/listTypesDiets.js
@@ -0,0 +1,15 @@
+const listTypesDietsDefault = [
+    "Gluten Free",
+    "Ketogenic",
+    "Vegetarian",
+    "Lacto-Vegetarian",
+    "Ovo-Vegetarian",
+    "Vegan",
+    "Pescetarian",
+    "Paleo",
+    "Primal",
+    "Low FODMAP",
+    "Whole30"
+];
+
+module.exports = {listTypesDietsDefault}
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const recipe = require('./recipe.js');
+const diet = require('./diet.js');
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use('/recipes', recipe);
+router.use('/diets', diet);
 
 module.exports = router;
